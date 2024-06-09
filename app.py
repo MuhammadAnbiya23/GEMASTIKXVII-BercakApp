79fb4db95175e3347ff9735f31d51275ca38493d
RendiRuswandi <rendiforbetter10@gmai.com>
2024-06-09 14:11:59 +0700

    memperbaiki BE sebelum memasukan FLask

--- app.py
+++ app.py
@@ -9,22 +9,18 @@ api = Api(app)
 bcrypt = Bcrypt(app)
 db = MySQL(app)
 
-# Konfigurasi database
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'db_gemastik'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-
-# Konfigurasi aplikasi
 app.config["SECRET_KEY"] = 'RahasiaKabupatenSukabumi'
 app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 
 db.init_app(app)
 
-# Memuat rute
 from routes import *
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -6,52 +6,32 @@ class DataUser:
     def create_user(name, email, password):
         cursor = db.connection.cursor()
         try:
-            # hashed_password = bcrypt.hashpw(password, bcrypt.gensalt()).decode('utf-8') 
             hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-            cursor.execute("INSERT INTO data_user (name, email, password) VALUES (%s, %s, %s)", 
+            cursor.execute("INSERT INTO data_users (name, email, password) VALUES (%s, %s, %s)", 
                            (name, email,hashed_password))
             db.connection.commit()
-            print(f"User {name} created successfully.")  # Debugging statement
+            print(f"User {name} created successfully.")
         except Exception as e:
             print(f"Error: {e}")
             db.connection.rollback()
-        # finally:
-        #     cursor.close()
+        finally:
+            cursor.close()
 
     @staticmethod
     def get_user_by_email(email):
         cursor = db.connection.cursor()
         try:
-            cursor.execute("SELECT * FROM data_user WHERE email = %s", (email,))
+            cursor.execute("SELECT * FROM data_users WHERE email = %s", (email,))
             user = cursor.fetchone()
-            print(f"Fetched user: {user}")  # Debugging statement
+            print(f"Fetched user: {user}")
             return user
         except Exception as e:
             print(f"Error: {e}")
             return None
-        # finally:
-        #     cursor.close()
-
-    @staticmethod
-    def get_hashed_password(email):
-        cursor = db.connection.cursor()
-        try:
-            cursor.execute("SELECT password FROM data_user WHERE email = %s", (email,))
-            hashed_password = cursor.fetchone()
-            if hashed_password:
-                return hashed_password[0]
-            else:
-                return None
-        except Exception as e:
-            print(f"Error: {e}")
-            return None
-        # finally:
-            # cursor.close()
+        finally:
+            cursor.close()
 
     @staticmethod
     def check_password(stored_password, provided_password):
         return bcrypt.check_password_hash(stored_password, provided_password)
-    # def check_password(password) :
-        # hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
-        # return bcrypt.checkpw(password, hashed_password)
 
